0c21d660b9ede5fd65d0fd6d99a17d86c7c76b91
d86leader <d86leader@mail.com>
2019-04-29 16:25:18 +0700

    added handlers for line selection

--- src/Edit/Execute.hs
+++ src/Edit/Execute.hs
@@ -2,23 +2,46 @@ module Edit.Execute
 (
 ) where
 
-import Data.Map.Strict (member, insert)
+
+import Data.Map.Strict (Map, member, insert, union, delete, keysSet)
+import qualified Data.Set as Set
+import qualified Data.Map.Strict as Map
 import qualified Edit.Command as Command
-import Edit.Command (Command(..), LinewiseMovement(..), CharacterMovement(..),
-                     VSide(..), HSide(..))
-import Edit.Effects (Buffer(..), Effects(..), EditAtom, newCursor)
+import Edit.Command (Command(..), LinewiseMovement(..), CharacterMovement(..)
+                    ,VSide(..), HSide(..))
+import Edit.Effects (Buffer(..), Effects(..), EditAtom, newCursor
+                    ,editBody, editFileName, editCursors)
+
 
 -- |Execute editor commands to modify a buffer
 
 runCommand :: Command -> Buffer -> EditAtom
 runCommand (AddLineSelection movement) = addLineSelection movement
+runCommand (RemoveLineSelection movement) = removeLineSelection movement
 
+
+--- Line commands ---
+
+incLines :: (Num a, Eq a) => a -> Map a b -> Map a b
+incLines x = Map.fromAscList . map (addToFst x) . Map.toAscList
+    where addToFst x (a, b) = (a + x, b)
 
 addLineSelection :: LinewiseMovement -> Buffer -> EditAtom
-addLineSelection (AbsoluteNumber line) buffer =
-    let map = cursors buffer
-    in if line `member` map
-       then return buffer
-       else let inserted = insert line newCursor map
-                updated = buffer {cursors = inserted}
-            in return updated
+addLineSelection (AbsoluteNumber line) =
+    return . editCursors (addLine line)
+    where addLine line cur = if line `member` cur
+                             then cur
+                             else insert line newCursor cur
+addLineSelection (RelativeNumber offset) =
+    return . editCursors (addLines offset)
+    where addLines offset cur = let addedCur = incLines offset cur
+                                in union cur addedCur
+
+removeLineSelection :: LinewiseMovement -> Buffer -> EditAtom
+removeLineSelection (AbsoluteNumber line) =
+    return . editCursors (delete line)
+removeLineSelection (RelativeNumber offset) =
+    return . editCursors (deleteLines offset)
+    where deleteLines offset cur =
+            let toDel = Set.map (+ offset) $ keysSet cur
+            in Map.withoutKeys cur toDel
