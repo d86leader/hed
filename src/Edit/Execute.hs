652e9b2602837eff59e52eba7694d39d84f4bb37
d86leader <d86leader@mail.com>
2019-04-30 15:39:50 +0700

    wrote line deleter

--- src/Edit/Execute.hs
+++ src/Edit/Execute.hs
@@ -3,9 +3,10 @@ module Edit.Execute
 ) where
 
 
-import Data.Map.Strict (Map, member, insert, union, delete, keysSet)
+import Data.Map.Strict (Map, member, insert, union, delete, keysSet, keys)
 import qualified Data.Set as Set
 import qualified Data.Map.Strict as Map
+import qualified Data.Vector as Vector
 import qualified Edit.Command as Command
 import Edit.Command (Command(..), LinewiseMovement(..), CharacterMovement(..)
                     ,VSide(..), HSide(..))
@@ -19,11 +20,13 @@ runCommand :: Command -> Buffer -> EditAtom
 runCommand (AddLineSelection movement) = addLineSelection movement
 runCommand (RemoveLineSelection movement) = removeLineSelection movement
 
+runCommand DeleteLines = deleteLines
 
---- Line commands ---
+
+--- Line selection commands ---
 
 incLines :: (Num a, Eq a) => a -> Map a b -> Map a b
-incLines x = Map.fromAscList . map (addToFst x) . Map.toAscList
+incLines x = Map.fromAscList . map (addToFst x) . Map.toAscList -- mapKeys
     where addToFst x (a, b) = (a + x, b)
 
 addLineSelection :: LinewiseMovement -> Buffer -> EditAtom
@@ -45,3 +48,14 @@ removeLineSelection (RelativeNumber offset) =
     where deleteLines offset cur =
             let toDel = Set.map (+ offset) $ keysSet cur
             in Map.withoutKeys cur toDel
+
+
+--- Line editing commands ---
+
+deleteLines :: Buffer -> EditAtom
+deleteLines Buffer{body = lines , cursors = cur, filename = fname} =
+    let numbers = keys cur
+        dropAll = foldr (.) id (map Vector.drop numbers)
+        dropped = dropAll lines
+        resetCursor = Map.fromAscList [(1, newCursor)]
+    in return Buffer{body = dropped, cursors = resetCursor, filename = fname}
